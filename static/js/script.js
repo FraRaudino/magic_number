f6273a755fb1f9c035e2f7e81d422914e73cb3f4
FraRaudino <francescoraudino136@gmail.com>
2024-10-20 19:42:01 +0200

    finito

--- static/js/script.js
+++ static/js/script.js
@@ -21,23 +21,31 @@ function magicNumber(event){
     {
         console.log("valore non valido")
         Indovinato=false;
+        elem1.innerHTML=`Non hai inserito nessun numero!`;
+        elem2.innerHTML=`Inserisci un numero valido`;
     }else if(n>nGenerato)
     {
         console.log("Il numero inserito è troppo grande");
         nTentativi=nTentativi-1;
+        elem1.innerHTML=`Il numero <strong>${n}</strong> è troppo grande`;
+        elem2.innerHTML=`Hai ancora a disposizione <strong>${nTentativi}</strong> tentativi`
     }else if(n<nGenerato)
     {
         console.log("Il numero inserito è troppo piccolo");
         nTentativi=nTentativi-1;
+        elem1.innerHTML=`Il numero <strong>${n}</strong> è troppo piccolo`;
+        elem2.innerHTML=`Hai ancora a disposizione <strong>${nTentativi}</strong> tentativi`
     }else if(n===nGenerato)
     {
         console.log("HAI VINTO!!!");
         Indovinato=true;
+        elem1.innerHTML=`Bravo, hai vinto!!`;
+        elem2.innerHTML=`Hai usato <strong>${nTentativi}</strong> tentativi`
     }
     if(nTentativi===0 && !Indovinato)
     {
-        console.log("Hai esaurito i tentativi!")
-        console.log("Il numero era" + nGenerato)
+        elem1.innerHTML=`Mi dispiace hai esaurito i tentativi!!`;
+        elem2.innerHTML=`Il numero era <strong>${nGenerato}</strong>`
     }
 
 }
@@ -46,6 +54,8 @@ let nTentativi=5;
 let Indovinato=false;
 let nGenerato=generateRandomNumber();
 let n;
+let elem1=document.querySelector("#text2")
+let elem2=document.querySelector("#text3")
 
 console.log("BENVENUTO A MAGIC NUMBER");
 let btn=document.querySelector("#guessBtn")
